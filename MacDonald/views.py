f26a0e56d6c672039592dba898e78555271c144d
ashukla-ops <ankeet.shukla@gmail.com>
2024-11-11 17:24:30 +0530

    second

---
+++ MacDonald/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from Template import *
+
+# Create your views here.
+
+def HomeView(request):
+    return render(request,'home.html')
+
+
+def AboutView(request):
+    return render(request,'about.html')
+
+
+def MenuView(request):
+    return render(request,'menu.html')
+
+
+def BookTableView(request):
+    return render(request,'book_table.html')
